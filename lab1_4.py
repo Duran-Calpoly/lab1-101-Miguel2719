851816270f3e8d331f8491c777cdf5ca72298eb7
Miguel2719 <miguelangelorozco075@gmail.com>
2026-01-17 22:59:13 +0000

    Lab 1 Done

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,25 @@
-pass
+#Code #1
+def calculate_average(num1, num2, num3):
+    return(num1 + num2 + num3) / 3
+
+avg = calculate_average(10, 20, 30)
+
+print("Average: ", avg)
+
+
+#Code #2
+def add_tax(amount):
+    return(amount * 1.10)
+
+bill_total = add_tax(10)
+
+print("Total: ", bill_total)
+
+
+#Code #3
+def greet_user(name):
+    return(name)
+
+name = input("What is your name?")
+
+print("Hello", name)
--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,35 @@
-pass
+#Code #1
+def check_multiple(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return True
+    else:
+        return False
+
+print(check_multiple(15))
+
+
+#Code #2
+def check_password(input_string):
+    secret_code = "CSC101"
+
+    if input_string == secret_code:
+        return "Access granted"
+    else:
+        return "Access denied"
+
+user_input = input("Enter the secret code: ")
+result = check_password(user_input)
+print(result)
+
+#Code #3
+def calculate_federal_tax(salary):
+    if salary <= 11000:
+        return salary * 0.10
+    elif salary > 11000 and salary <= 44725:
+        return salary * 0.12
+    elif salary > 44725 and salary <= 95375:
+        return salary * 0.22
+    elif salary > 95375:
+        return salary * 0.24
+    
+print(calculate_federal_tax(11000))
